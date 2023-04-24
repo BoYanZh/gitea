cc082465e2cf7e374b2647b442c7f856e6d33d60
BoYanZh <boyanzh233@gmail.com>
2023-04-24 21:38:08 +0800

    feat: wiki activity

--- routers/web/repo/activity.go
+++ routers/web/repo/activity.go
@@ -55,13 +55,25 @@ func Activity(ctx *context.Context) {
 		ctx.Repo.CanRead(unit.TypeReleases),
 		ctx.Repo.CanRead(unit.TypeIssues),
 		ctx.Repo.CanRead(unit.TypePullRequests),
-		ctx.Repo.CanRead(unit.TypeCode)); err != nil {
+		ctx.Repo.CanRead(unit.TypeCode), false); err != nil {
 		ctx.ServerError("GetActivityStats", err)
 		return
+	}
+	if ctx.Repo.Repository.HasWiki() {
+		if ctx.Data["WikiActivity"], err = activities_model.GetActivityStats(ctx, ctx.Repo.Repository, timeFrom,
+			false, false, false, true, true); err != nil {
+			ctx.ServerError("GetActivityStats(wiki)", err)
+			return
+		}
 	}
 
 	if ctx.PageData["repoActivityTopAuthors"], err = activities_model.GetActivityStatsTopAuthors(ctx, ctx.Repo.Repository, timeFrom, 10); err != nil {
 		ctx.ServerError("GetActivityStatsTopAuthors", err)
+		return
+	}
+
+	if ctx.PageData["repoWikiActivityTopAuthors"], err = activities_model.GetActivityStatsTopAuthorsByRepoPath(ctx, ctx.Repo.Repository.WikiPath(), timeFrom, 10); err != nil {
+		ctx.ServerError("GetActivityStatsTopAuthors(wiki)", err)
 		return
 	}
 

50e38595f4ae72a76fa94ca7c59b9d4ef498c3ea
BoYanZh <boyanzh233@gmail.com>
2024-10-31 23:26:44 -0400

    fix: remove unnecessary migration fields

--- models/migrations/v1_23/v307.go
+++ models/migrations/v1_23/v307.go
@@ -4,20 +4,10 @@
 package v1_23 //nolint
 
 import (
-	issues_model "code.gitea.io/gitea/models/issues"
-	repo_model "code.gitea.io/gitea/models/repo"
-	user_model "code.gitea.io/gitea/models/user"
 	"code.gitea.io/gitea/modules/timeutil"
 
 	"xorm.io/xorm"
 	"xorm.io/xorm/schemas"
-)
-
-type (
-	// NotificationStatus is the status of the notification (read or unread)
-	NotificationStatus uint8
-	// NotificationSource is the source of the notification (issue, PR, commit, etc)
-	NotificationSource uint8
 )
 
 type improveNotificationTableIndicesAction struct {
@@ -25,19 +15,14 @@ type improveNotificationTableIndicesAction struct {
 	UserID int64 `xorm:"INDEX NOT NULL"`
 	RepoID int64 `xorm:"INDEX NOT NULL"`
 
-	Status NotificationStatus `xorm:"SMALLINT INDEX NOT NULL"`
-	Source NotificationSource `xorm:"SMALLINT INDEX NOT NULL"`
+	Status uint8 `xorm:"SMALLINT INDEX NOT NULL"`
+	Source uint8 `xorm:"SMALLINT INDEX NOT NULL"`
 
 	IssueID   int64  `xorm:"INDEX NOT NULL"`
 	CommitID  string `xorm:"INDEX"`
 	CommentID int64
 
 	UpdatedBy int64 `xorm:"INDEX NOT NULL"`
-
-	Issue      *issues_model.Issue    `xorm:"-"`
-	Repository *repo_model.Repository `xorm:"-"`
-	Comment    *issues_model.Comment  `xorm:"-"`
-	User       *user_model.User       `xorm:"-"`
 
 	CreatedUnix timeutil.TimeStamp `xorm:"created INDEX NOT NULL"`
 	UpdatedUnix timeutil.TimeStamp `xorm:"updated INDEX NOT NULL"`
